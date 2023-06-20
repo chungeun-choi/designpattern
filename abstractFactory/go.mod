bb447851c4d0c9f0105970ca29c5c81db3a46dd8
cucuridas@gamil.com <3310223@naver.com>
2023-06-20 15:01:15 +0900

    추상 팩토리 실습 완료

---
+++ abstractFactory/go.mod
@@ -0,0 +1,3 @@
+module github.com/designpattern/abstractFactory
+
+go 1.19
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
+	af "github.com/designpattern/abstractFactory"
 	. "github.com/designpattern/factory"
 )
 
 
 func main() {
-	factory()
+	//factory()
+	abstractFactory()
 }
 
 func printDetails(c CarProduct){
@@ -27,3 +29,19 @@ func factory() {
 	printDetails(genesis)
 	printDetails(k3)
 }
+
+
+func abstractFactory() {
+	//추상 팩토리인 가구팩토리를 초기화 - Modern으로 생성
+	furniture,_ := af.GetFurniture("modern")
+
+	mc := furniture.MakeChair() 
+	md := furniture.MakeDesk()
+	ms := furniture.MakeSopa()
+
+	fmt.Printf("Moder Chair category: %s",mc.GetCategory())
+	fmt.Println()
+	fmt.Printf("Moder Desk category: %s",md.GetCategory())	
+	fmt.Println()
+	fmt.Printf("Moder Sopa category: %s",ms.GetCategory())
+}	
